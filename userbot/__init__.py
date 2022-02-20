547f7e261ae4bd7951a07494a00fbf81ccf2fb54
kenaja <99611859+kenaja@users.noreply.github.com>
2022-02-20 13:31:42 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -150,7 +150,7 @@ GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/Tonic990/Tonic-Userbot")
+    "https://github.com/kenaja/Tonic-Userbot")
 UPSTREAM_REPO_BRANCH = os.environ.get(
     "UPSTREAM_REPO_BRANCH", "Tonic-Userbot")
 
